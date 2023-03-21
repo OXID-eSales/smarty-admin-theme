9292b47eb7f2c4190b5a519513d70fbce7217624
Daniil Tkachev <daniil.tkachev@oxid-esales.com>
2023-03-21 15:10:42 +0100

    OXDEV-6529 Change button text snippet

--- en/lang.php
+++ en/lang.php
@@ -1504,7 +1504,7 @@ $aLang = [
     'mxferoles'                => 'Shop Roles',
     /* tabs */
     'tbclorder_iPayment'       => 'IPayment',
-    'tbclnewsletter_recipients' => 'Export recipients',
+    'tbclnewsletter_recipients' => 'Export users',
     'tbcl_main'                => 'Main',
     'tbcladminlinks_mall'      => 'Mall',
     'tbcldelivery_mall'        => 'Mall',
